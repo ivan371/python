4a17b2a67d7694267b8f6864c31cab2fb08de36e
Ivan <nagaiko.ivan@gmail.com>
2017-03-13 00:20:30 +0300

    work

--- BFS/C1.py
+++ BFS/C1.py
@@ -11,17 +11,19 @@ def dijkstra(G, start):
         for neighbour in G[current]:
             l = d[current] + G[current][neighbour]
             if l < d[neighbour]:
-                d[neighbour] = 1
-                heappush(Q, (1, neighbour))
+                d[neighbour] = l
+                heappush(Q, (l, neighbour))
         used.add(current)
     return d
 
 n, m, x, y = map(int,input().split())
 d = {}
+for i in range(n):
+    d[i] = {}
 for i in range(m):
     u, v = map(int, input().split())
-    d[u] = {v: 1}
-    d[v] = {u: 1}
+    d[u][v] = 1
+    d[v][u] = 1
 
 dic = dijkstra(d, x)
 print(dic[y])
