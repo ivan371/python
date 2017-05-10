1a96db60fbc1ffd65eb9e33758ab7934d00f8064
ivan371 <ivan371@users.noreply.github.com>
2017-05-10 16:37:05 +0300

    Update 10.py

--- КР/10.py
+++ КР/10.py
@@ -2,6 +2,8 @@ s = input()
 l = len(s)
 for i in range(l):
     j = 0
-    while(s[i-j:i] == s[i+1:i+j+1] and j <= i and i + j < l):
+    a = s[i+1:i+j+1]
+    while(s[i-j:i] == a[::-1] and j <= i and i + j < l):
         j+=1
+        a = s[i+1:i+j+1]
     print(2*j - 1, end=' ')
